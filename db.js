ca224d351413fb63a20e025c63ae9338e84bf632
User <user@example.com>
2026-03-09 23:08:59 +0700

    Configure cloud database connection

--- db.js
+++ db.js
@@ -2,11 +2,7 @@ require('dotenv').config();
 const { Pool } = require('pg');
 
 const pool = new Pool({
-  user: process.env.DB_USER || 'postgres',
-  host: process.env.DB_HOST || 'localhost',
-  database: process.env.DB_NAME || 'wedding',
-  password: process.env.DB_PASSWORD || 'Df_262009',
-  port: process.env.DB_PORT || 5432,
+  connectionString: process.env.DATABASE_URL,
 });
 
 module.exports = {
